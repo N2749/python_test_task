5debc2bdb538ffc21997370a14d705299760eda1
Nurbolat <nurba.dav@gmail.com>
2023-03-06 16:20:46 +0600

    feat: raw nesting

--- text_edit/models.py
+++ text_edit/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Text(models.Model):
+    position = models.IntegerField
+    text = models.CharField(max_length=200)
+    Parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name='+', null=True, default=None, blank=True)
+
+    # function takes primary key of Text object, and should return number
+    # i.e PK (3)
+    # return value 1.1.1
+    # core-idea: there is need for the function that counts the number of this child of the parent,
+    # and then combine it with the number of parents
+    def get_nesting_number():
+        
+        return
